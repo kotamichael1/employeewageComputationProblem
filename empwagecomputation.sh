bee660ab96eae29a7e000743ff4b89d6f6055ea1
“MICHAELKOTAgit config  user.namegit config --global user.name “MICHAELKOTA <kotamichael11@gmail.com>
2022-11-20 19:16:36 +0530

    Welcome to Employee Wage Computation program

---
+++ empwagecomputation.sh
@@ -0,0 +1,10 @@
+echo "welcome to Employee wage computation program"
+
+ispresent=1;
+randomCheck=$((RANDOM%2));
+if [ $ispresent -eq $randomCheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
