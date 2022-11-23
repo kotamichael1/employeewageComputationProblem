26c1727269dd250aeddfef213f1d3052187cdaca
“MICHAELKOTAgit config  user.namegit config --global user.name “MICHAELKOTA <kotamichael11@gmail.com>
2022-11-23 10:52:07 +0530

    Added: Calculate Daily Employee wage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,6 +5,10 @@ randomCheck=$((RANDOM%2));
 if [ $ispresent -eq $randomCheck ]
 then
 echo "Employee is present"
+empratePerHr=20
+salary=$(($empHrs*$empRatePerHr))
+echo "salary="$salary
 else
 echo "Employee is absent"
+echo "salary="$salary
 fi
